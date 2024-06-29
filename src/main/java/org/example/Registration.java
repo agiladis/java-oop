e4713bea047f6d38d5199dcb43799e20f0d741e6
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-06-29 23:55:10 +0700

    [UPDATE] detailed comment about interface

--- src/main/java/org/example/Registration.java
+++ src/main/java/org/example/Registration.java
@@ -1,5 +1,11 @@
 package org.example;
 
+/* INTERFACE:
+    - hanya berisi abstract method
+    - ga punya field
+    - ga punya constructor
+    - ga bisa di-instance
+* */
 public interface Registration {
     void registration(String name);
     Person getRegisteredPerson(String name);
