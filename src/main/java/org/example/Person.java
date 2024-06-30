94675efffb1f662f29046159a7472312bf26025c
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-06-30 11:35:19 +0700

    [ADD] comment, characteristic about abstract and interface

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,22 +1,23 @@
 package org.example;
 
 /* ABSTRACT:
+    - gunakan ketika ada relasi "is-a" yang kuat antara kelas.
     - menggunakan keyword abstract
     - boleh ada abstract method : deklarasi method tanpa inmplementasi code
-    - boleh ada method non-abstract : TAPI abstract class gabisa di instance
-    - abstract class cuman bisa diinstace kalau abstract class sudah punya subclass
+    - boleh ada method non-abstract (konkrit) : TAPI abstract class gabisa di instance
+    - abstract class cuman bisa diinstace kalau melalui subclass dari abstract class
+    - boleh ada field dan constructor
 * */
 public abstract class Person {
-    private String name;
+    private String name; // variable instance
     private String gender;
 
-    public Person() {}
+    public Person() {} // constructor
 
-    // example of abstract class
-    public abstract void work();
+    public abstract void work(); // abstract method
 
     protected void changeName(String newName)
     {
         this.name = newName;
-    }
+    } // non-abstract or concrete method
 }
