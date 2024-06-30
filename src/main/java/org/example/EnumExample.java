541283d65638249f022b331c1e4c3bc53ba676cf
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-07-01 00:38:10 +0700

    [ADD] an example of enum

---
+++ src/main/java/org/example/EnumExample.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/* ENUM:
+    - berisi konstanta
+    - bisa memiliki metode dan atribut/field
+* */
+public class EnumExample {
+    enum Season {
+        WINTER(5), SPRING(10), SUMMER(15), FALL(20);
+
+        private int value; // merupakan constant berupa angka
+        private Season(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (Season s : Season.values())
+            System.out.println(s + " " + s.value);
+    }
+}
