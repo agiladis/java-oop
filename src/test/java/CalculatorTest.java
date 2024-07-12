0cd37a6bb97886655163cf565947106bf996fc68
Agil Adi Saputro <agiladisaputro@gmail.com>
2024-07-12 16:09:46 +0700

    [ADD] beforeEach implementation

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,21 @@
 import com.sun.jdi.request.InvalidRequestStateException;
 import org.example.Calculator;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setup() {
+        calculator = new Calculator();
+    }
+
     @Test
     @DisplayName("Positive Test - Successful Addition")
     void testAdditionSuccess() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(5, 10);
         Assertions.assertEquals(15, result);
     }
@@ -16,7 +23,6 @@ public class CalculatorTest {
 //    @Test
 //    @DisplayName("Negative Test - Invalid Request
 //    void testInvalidRequestAddition() {
-//        Calculator calculator = new Calculator();
 //        Exception e = Assertions.assertThrows(InvalidRequestStateException.class, () -> calculator.add(null, null));
 //        Assertions.assertEquals("Request cannot be null", e.getMessage());
 //    }
